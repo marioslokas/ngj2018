99a108a1630b32ee0e170c48f87539c3817db6f7
Mathias Larsen <mathiasdodo@hotmail.com>
2018-04-14 16:33:46 +0200

    - set in a field for shape

--- Assets/PersonBehavior.cs
+++ Assets/PersonBehavior.cs
@@ -2,35 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PersonBehavior : MonoBehaviour {
+public class PersonBehavior : MonoBehaviour
+{
+    private bool isOnPlatform;
+    private bool isOnCrane;
+    private bool isOnTrain;
 
+    public Shapes MyShape { get; private set; }
 
+    private void Awake()
+    {
+        MyShape = (Shapes)Random.Range(1, 4);
 
-	bool isOnPlatform;
-	bool isOnCrane;
-	bool isOnTrain;
+        Debug.Log(MyShape);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    // Use this for initialization
+    private void Start()
+    {
+    }
 
-	void OnCollisionEnter(Collision other)
-	{
-		if (other.collider.gameObject.tag.Equals("Platform")) {
-			isOnPlatform = true;
-		}
-	}
+    // Update is called once per frame
+    private void Update()
+    {
+    }
 
-	void OnCollisionExit(Collision other)
-	{
-		if (other.collider.gameObject.tag.Equals("Platform")) {
-			isOnPlatform = false;
-		}
-	}
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.gameObject.tag.Equals("Platform"))
+        {
+            isOnPlatform = true;
+        }
+    }
 
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.collider.gameObject.tag.Equals("Platform"))
+        {
+            isOnPlatform = false;
+        }
+    }
 }
