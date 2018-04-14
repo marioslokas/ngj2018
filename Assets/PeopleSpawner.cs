5563738e362d17fd4a0c130553cc43989ea99d57
Unknown <marioslokas@gmail.com>
2018-04-14 13:48:46 +0200

    Initial work on people

---
+++ Assets/PeopleSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleSpawner : MonoBehaviour {
+
+	public GameObject[] spawnPoints;
+
+	public float spawningInterval = 10f;
+	public bool canSpawn = true;
+
+	public GameObject personPrefab;
+
+	private GameObject spawningPoint;
+
+	private float currentElapsedTime;
+
+	// Use this for initialization
+	void Start () {
+		currentElapsedTime = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentElapsedTime += Time.deltaTime;
+		if (currentElapsedTime > spawningInterval && canSpawn) {
+
+			GameObject spawnPoint = PickSpawnPoint ();
+
+			GameObject newSphere = Instantiate (personPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation) as GameObject;
+
+			currentElapsedTime = 0f;
+		}
+	}
+
+	GameObject PickSpawnPoint()
+	{
+		int spawnPointsNumber = spawnPoints.Length;
+		int randomPoint = UnityEngine.Random.Range (0, spawnPointsNumber);
+
+		return spawnPoints [randomPoint];
+
+	}
+}
---
+++ Assets/PersonBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonBehavior : MonoBehaviour {
+
+
+
+	bool isOnPlatform;
+	bool isOnCrane;
+	bool isOnTrain;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.collider.gameObject.tag.Equals("Platform")) {
+			isOnPlatform = true;
+		}
+	}
+
+	void OnCollisionExit(Collision other)
+	{
+		if (other.collider.gameObject.tag.Equals("Platform")) {
+			isOnPlatform = false;
+		}
+	}
+
+}
